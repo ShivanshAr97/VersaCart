e9f15ca7535fd303fd89a8f86b0fa99742394d69
ShivanshAr97 <shivansh.arora973@gmail.com>
2024-06-13 17:17:21 +0530

    options

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,46 +1,80 @@
-import React from 'react'
-import { useShoppingCart } from '../context/CartContext'
-import { useFilterCart } from '../context/FilterContext'
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+import React, { useEffect, useState } from "react";
+import { useShoppingCart } from "../context/CartContext";
+import { useFilterCart } from "../context/FilterContext";
+import fileContent from "../data/file.json";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 export default function Options() {
+  const [val, setVal] = useState(250);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setData(fileContent);
+  }, []);
+
+  const category = data.map((item) => item.category);
+  const uniqCat = [...new Set(category)];
+  const price = data.map((item) => item.price);
+  const color = data.map((item) => item.color);
+  const uniqColor = [...new Set(color)];
+  const uniqSize = ["S", "M", "L", "XL"];
+
+  let a = Math.min(...price);
+  let b = Math.max(...price);
+
+  console.log({ uniqSize, uniqCat, uniqColor });
   return (
-    <div className='flex flex-col text-lg'>
-        <br />
-        <p className='text-xl font-bold my-4'>Filters</p>
+    <div className="flex flex-col text-lg">
+      <br />
+      <p className="text-xl font-bold my-4">Filters</p>
 
-        <p>Price</p>
-        <p>Brand</p>
-        <p>Color</p>
-        <p>Category</p>
-        <p>Size</p>
-        <p>Rating</p>
+      <p className="mt-4 font-semibold">Price Range</p>
+      <div>
+        <span>{a}</span>
+        <input
+          type="range"
+          id="vol"
+          name="vol"
+          min={a}
+          max={b}
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <span>{b}</span>
+        Val: {val}
+      </div>
 
-
-        <p className='my-1 font-semibold'>Sort</p>
-        <div>
-            <input type="radio" id="ascending" name="order" value="Asc"/>
-            <label htmlFor="ascending">Ascending</label>
+      <p>Color</p>
+      {uniqColor.map((item, index) => (
+        <div key={index} className="flex gap-2 items-center align-middle">
+          <p className="border"
+            style={{
+              width: "20px",
+              height: "20px",
+              backgroundColor:(`${item}`)
+            }}
+          ></p>
+          <p>{item}</p>
         </div>
-        <div>
-            <input type="radio" id="descending" name="order" value="Desc"/>
-            <label htmlFor="descending">Descending</label>
+      ))}
+      <p>Category</p>
+      {uniqCat.map((item, index) => (
+        <div key={index} className="flex gap-2 items-center align-middle">
+          <p>{item}</p>
         </div>
+      ))}
+      <p>Size</p>
+      {uniqSize.map((item, index) => (
+        <div key={index} className="flex gap-2 items-center align-middle">
+          <p>{item}</p>
+        </div>
+      ))}
+      <p>Rating</p>
+      <p>Top Rated</p>
+      
 
-        <p className='mt-4 font-semibold'>Price Range</p>
-        <div>
-            <input type="range" id="vol" name="vol" min="0" max="50"/>
-        </div>
-
-        <p className='mt-4 mb-2 font-semibold'>Rating</p>
-        <div className='flex'>
-            <p className='cursor-pointer'><AiOutlineStar/></p>
-            <p className='cursor-pointer mx-1'><AiOutlineStar/></p>
-            <p className='cursor-pointer mx-1'><AiOutlineStar/></p>
-            <p className='cursor-pointer mx-1'><AiOutlineStar/></p>
-            <p className='cursor-pointer mx-1'><AiOutlineStar/></p>
-        </div>
-        <button className='bg-red-500 py-1 my-4 text-lg rounded-md capitalize text-white'>clear filters</button>
+      <button className="bg-red-500 py-1 my-4 text-lg rounded-md capitalize text-white">
+        clear filters
+      </button>
     </div>
-  )
+  );
 }
--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,15 +1,15 @@
-import React, { useState,Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import { CurrFormater } from "../utils/CurrFormater";
 import { useContext } from "react";
-import {CartContext} from "../context/CartContext";
+import { CartContext } from "../context/CartContext";
 import { FilterContext } from "../context/FilterContext";
 import { FaCameraRetro } from "react-icons/fa";
 import Modal from "./Modal";
-import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
-import Apple from '../../public/Apple'
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import Apple from "../../public/Apple";
 
-const StoreItem=({
+const StoreItem = ({
   id,
   color,
   brand,
@@ -18,80 +18,113 @@ const StoreItem=({
   desc,
   category,
   fullName,
-  image
-})=> {
-  const {getItems, incItems, decItems, removeItems} = useContext(CartContext);
-  const {} = useContext(FilterContext)
+  rating,
+  size,
+  image,
+}) => {
+  const { getItems, incItems, decItems, removeItems } = useContext(CartContext);
+  const {} = useContext(FilterContext);
 
   const quant = getItems(id);
 
-  const [open, setOpen] = useState(false)
-  const [three, setThree] = useState(false)
-  const openModal=()=>{
-    setThree(!three)
-  }
+  const [open, setOpen] = useState(false);
+  const [three, setThree] = useState(false);
+  const openModal = () => {
+    setThree(!three);
+  };
 
   return (
     <div>
       <Modal open={open} onClose={() => setOpen(false)}>
-        <div className="text-center w-56">
-          <div className="mx-auto my-4 w-48">
-          <img className="w-60 mx-auto rounded-lg h-40 object-cover" src={image} alt="" />
-            <p className="text-sm text-gray-500">
-              Are you sure you want to delete this item?
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <button className="btn btn-danger w-full">Delete</button>
-            <button
+      <button
               className="btn btn-light w-full"
               onClick={() => setOpen(false)}
             >
               Cancel
             </button>
+        <div className="text-center w-[32rem]">
+          <div className="mx-auto my-4 w-48">
+            <img
+              className="w-60 mx-auto rounded-lg h-40 object-cover"
+              src={image}
+              alt=""
+            />
+            <div>
+              <h1>{product}</h1>
+              <p>{desc}</p>
+              <p>{brand}</p>
+              <p>{rating}</p>
+              <p>{color}</p>
+              <p>{size}</p>
+              <p>{price}</p>
+              <p>{category}</p>
+              <p>{fullName}</p>
+            </div>
+            
+          </div>
+          <div className="flex gap-4">
+            
           </div>
         </div>
       </Modal>
       <div className="relative">
-      {three ? (
-      <Canvas className=''>
-        <Suspense fallback={null}>
-          <OrbitControls enableZoom={false}/>
-          <ambientLight/>
-          <Apple scale={5}/>
-        </Suspense>
-        <Environment preset='sunset'/>
-      </Canvas>
-  ):
-      <button onClick={() => setOpen(true)}>
-        <img className="w-60 mx-auto rounded-lg h-40 object-cover" src={image} alt="" />
-      </button>
-        }
+        {three ? (
+          <Canvas className="">
+            <Suspense fallback={null}>
+              <OrbitControls enableZoom={false} />
+              <ambientLight />
+              <Apple scale={5} />
+            </Suspense>
+            <Environment preset="sunset" />
+          </Canvas>
+        ) : (
+          <button onClick={() => setOpen(true)}>
+            <img
+              className="w-60 mx-auto rounded-lg h-40 object-cover"
+              src={image}
+              alt=""
+            />
+          </button>
+        )}
         <div className="flex justify-between my-2">
           <span>{product}</span>
           <span>{CurrFormater(price)}</span>
         </div>
         {quant === 0 ? (
           <>
-          <button className="border px-4 py-1 flex mx-auto my-2 rounded-lg bg-green-600 text-white" onClick={()=>incItems(id)}>Add new item</button>
-          <button onClick={openModal} className="absolute top-0"><FaCameraRetro/></button>
+            <button
+              className="border px-4 py-1 flex mx-auto my-2 rounded-lg bg-green-600 text-white"
+              onClick={() => incItems(id)}
+            >
+              Add new item
+            </button>
+            <button onClick={openModal} className="absolute top-0">
+              <FaCameraRetro />
+            </button>
           </>
-          
         ) : (
           <div className="flex justify-between my-2">
             <div className="flex items-center align-middle">
-              <button className="" onClick={()=>incItems(id)}>+</button>
+              <button className="" onClick={() => incItems(id)}>
+                +
+              </button>
               <span className="border mx-2 px-1">{quant}</span>
-              <button onClick={()=>decItems(id)}>-</button>
+              <button onClick={() => decItems(id)}>-</button>
             </div>
-            <button className="border px-4 py-0.5 rounded-lg bg-red-500 text-white" onClick={()=>removeItems(id)}>Delete</button>
-            <button onClick={openModal} className="absolute top-0"><FaCameraRetro/></button>
+            <button
+              className="border px-4 py-0.5 rounded-lg bg-red-500 text-white"
+              onClick={() => removeItems(id)}
+            >
+              Delete
+            </button>
+            <button onClick={openModal} className="absolute top-0">
+              <FaCameraRetro />
+            </button>
           </div>
         )}
       </div>
-      
     </div>
   );
-}
+};
 
-export default StoreItem
+export default StoreItem;
